52d40367bdbacea26c00b52abd115a86a37c85a0
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2020-04-26 18:24:41 +0300

    Create Queue.h

---
+++ lab 1/Queue.h
@@ -0,0 +1,27 @@
+#pragma once
+
+struct Unit
+{
+    int value; // значение текущего элемента
+    Unit* prev = 0; // указатель на предыдущий эл-т
+};
+
+class Queue
+{
+private:
+    Unit* last = 0; // Указатель на конец очереди
+
+protected:
+    int size; // размер очереди
+    Unit* get_last(); // Возвращаем указатель на последний элемент для наследников
+
+public:
+    Queue(); // конструктор
+    ~Queue(); // деструктор
+    void push(int data); // добавление элемента в очередь
+    int pop(); // извлечения элемента
+    void out(); // вывод на экран
+    void copy(Queue& ob); // Копирование очереди
+    void merger(Queue& ob1, Queue& ob2); // Слияние очереди
+    bool check(); // Проверяем на пустоту очередь
+};
